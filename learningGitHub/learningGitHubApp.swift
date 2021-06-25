975314cd98dfa5141463568380e775f00f9ccd40
hotspicycook <redonfire29@gmail.com>
2021-06-25 16:11:49 -0500

    Initial Commit

---
+++ learningGitHub/learningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningGitHubApp.swift
+//  learningGitHub
+//
+//  Created by MyMac on 6/25/21.
+//
+
+import SwiftUI
+
+@main
+struct learningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
